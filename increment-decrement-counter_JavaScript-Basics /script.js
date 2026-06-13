f9f59840bc63bc8197c4531a4dde6ce5b65e1155
Muk-a-Dev <mukul.wok@gmail.com>
2026-06-13 15:50:12 +0530

    adding all progress in single repository

---
+++ increment-decrement-counter_JavaScript-Basics /script.js
@@ -0,0 +1,21 @@
+// function changeText(){
+//     document.getElementById("title").innerHTML = "Text Changed";
+// }
+
+
+// function showName(){
+//     document.getElementById("nameinput").value;
+//     document.getElementById("output").innerHTML = name;
+// }
+
+let counter = 0;
+
+function increment(){
+    counter++;
+    document.getElementById("count").innerHTML = counter;
+}
+
+function decrement(){
+    counter--;
+    document.getElementById("count").innerHTML = counter;
+}
